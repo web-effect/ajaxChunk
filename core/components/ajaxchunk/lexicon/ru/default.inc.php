a4aa025475f3b2d3528c6cac245cfba0e5ec7c5d
SintezCode <unarmedf@gmail.com>
2017-05-11 10:18:32 +0300

    Create default.inc.php

---
+++ core/components/ajaxchunk/lexicon/ru/default.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+$_lang['ajaxchunk'] = 'Ajax чанк';
+
+$_lang['ac.init_name_error'] = 'Уникальное имя чанка не указано';
+$_lang['ac.content_chunk_error'] = 'Чанк [[+chunk]] не найден';
+$_lang['ac.notavail'] = 'В данный момент сервер не доступен. Повторите попытку позже.';
