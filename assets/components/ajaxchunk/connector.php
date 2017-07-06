d26c92f291e4df3c7142aa678031851917d4b1ae
SintezCode <unarmedf@gmail.com>
2017-07-06 16:08:38 +0300

    Improved resource load
    Set request param, and tvs for plugins

--- assets/components/ajaxchunk/connector.php
+++ assets/components/ajaxchunk/connector.php
@@ -10,30 +10,50 @@ require_once MODX_CORE_PATH.'model/modx/modx.class.php';
 require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/index.php');*/
 $modx = new modX();
 
-/*$collections_cp = $modx->getOption('collections.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/collections/');
+$collections_cp = $modx->getOption('collections.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/collections/');
 if(file_exists($collections_cp))
 {
 	require_once $collections_cp.'model/collections/mysql/collectioncontainer.class.php';
 	require_once $collections_cp.'model/collections/collectioncontainer.class.php';
 	require_once $collections_cp.'model/collections/mysql/selectioncontainer.class.php';
 	require_once $collections_cp.'model/collections/selectioncontainer.class.php';
-}*/
+}
 
-
-if(is_numeric($_POST['resource']))$modx->resource = $modx->getObject('modResource',(int)$_POST['resource'],false);
+if($_POST['resource'])
+{
+    $modx->resource = $modx->getObject('modResource',(int)$_POST['resource']);
+}
 $modx->initialize( 'web' );
 $modx->invokeEvent("OnLoadWebDocument");
 $modx->lexicon->load($modx->getOption('cultureKey').':core:default');
+if($modx->resource)
+{
+	$q_var = $modx->getOption('request_param_alias', null, 'q');
+	$_REQUEST[$q_var]=$modx->makeUrl($modx->resource->id);
+	if($tvs=$modx->resource->getMany('TemplateVars', 'all')){
+		foreach ($tvs as $tv){
+			$modx->resource->set($tv->get('name'), array(
+				$tv->get('name'),
+				$tv->getValue($modx->resource->get('id')),
+				$tv->get('display'),
+				$tv->get('display_params'),
+				$tv->get('type'),
+			));
+		}
+	}
+}
 
 $result = array();
 $result['timestamp'] = (int)$_POST['timestamp'];
-if(!isset($_POST['ajaxchunk_name'])){
+if(!isset($_POST['ajaxchunk_name']))
+{
     $result['success'] = false;
-    $result['error'] = 'Identifer missed.';
+    $result['error'] = 'FFFFUUUUUUUUUUUUUUUUUU! identifier missed';
 }
-elseif(!isset($_SESSION['ajaxchunk_'.$_POST['ajaxchunk_name']])){
+elseif(!isset($_SESSION['ajaxchunk_'.$_POST['ajaxchunk_name']]))
+{
     $result['success'] = false;
-    $result['error'] = 'Indetifer not inited';
+    $result['error'] = 'FFFFUUUUUUUUUUUUUUUUUU! identifier is wrong';
 }
 else
 {
@@ -43,14 +63,15 @@ else
     $ajaxChunk = $modx->getService('ajaxChunk','ajaxChunk',MODX_CORE_PATH.'components/ajaxchunk/model/ajaxchunk/');
     $ajaxChunk->initialize($scriptProperties);
 
-    if($ajaxChunk->hasErrors){
+    if($ajaxChunk->hasErrors)
+    {
         $result['success'] = false;
         $result['error'] = $ajaxChunk->getErrors();
     }
-    else{
+    else
+    {
         $result['content'] = $ajaxChunk->getContent();
     }
-    $result['resource'] = $modx->resource->id;
 }
 
 echo json_encode($result);
